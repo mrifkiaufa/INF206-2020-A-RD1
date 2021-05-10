dad647cfea7217ebba70355dabd930d7af9b5fac
Muhammad Rifki Aufa <rifki_aufa@mhs.unsyiah.ac.id>
2021-05-10 16:16:00 +0700

    Memperbaru halaman home

--- ezakat/resources/views/home/index.blade.php
+++ ezakat/resources/views/home/index.blade.php
@@ -3,12 +3,12 @@
 
 @section('isi')
     <div class="container ms-3 mt-5">
-        <h1>Website Zakat Desa Ie masen</h1>
-        <p>Memudahkan proses pembayaran <br>
+        <h1 class="artikel">Website Zakat Desa Ie masen</h1>
+        <p class="artikel">Memudahkan proses pembayaran <br>
         dan penerimaan zakat bagi <br>
         warga-warga Ie Masen</p>
 
-        <p>Memudahkan semua golongan <br>
+        <p class="artikel">Memudahkan semua golongan <br>
         dalam melakukan rukun Islam di  <br>
         bulan yang suci</p>
     </div>
