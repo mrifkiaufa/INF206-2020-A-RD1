b0f780496ca48ac55653a9f051950cb2f3d1eca3
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2021-05-12 00:06:49 +0700

    Menambah file about

---
+++ ezakat/resources/views/about/about.blade.php
@@ -0,0 +1,15 @@
+@extends('template.template')
+@section('title','About')
+
+@section('isi')
+    <div class="container ms-3 mt-5">
+        <h1 class="artikel">Tentang Kami </h1>
+        <p class="artikel">Website ini dibuat untuk memudahkan <br>
+        proses pembayaran dan penerimaan zakat <br>
+        bagi warga desa Ie Masen</p>
+
+        <p class="artikel">Website ini dikelola oleh amil <br>
+        zakat desa Ie Masen<br>
+        </p>
+    </div>
+@endsection
