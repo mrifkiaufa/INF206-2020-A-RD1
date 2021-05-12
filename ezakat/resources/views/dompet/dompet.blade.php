66905eae65faf666db6edf888d97e90de556ae1f
Muhammad-Saifullah-Sani <81971527+Muhammad-Saifullah-Sani@users.noreply.github.com>
2021-05-12 09:47:48 +0700

    Menambahkan halaman dompet.

---
+++ ezakat/resources/views/dompet/dompet.blade.php
@@ -0,0 +1,18 @@
+@extends('template.template')
+@section('title','About')
+
+@section('isi')
+    <div class="container">
+        <h1 class="pt-5" style="color: dodgerblue; font-weight: bold;">Dompet Penerima Zakat</h1>
+        <div class="card mt-5" style="width: 35rem; border-color: dodgerblue; border-radius: 5%; background-color: rgba(14, 159, 221, 0.35);">
+            <div class="card-body">
+                <h5 class="card-tittle p-3">No KK : 729123498721</h5>
+                <h5 class="card-tittle p-3">Username : Saiful Anwar</h5>
+                <h5 class="card-tittle p-3">Email : saiful@gmail.com</h5>
+                <h5 class="card-tittle p-3">Jumlah Anggota Keluarga : 5</h5>
+                <h5 class="card-tittle p-3">Alamat : Ie Masen</h5>
+                <h5 class="card-tittle p-3">Saldo : Rp 1.500.000</h5>
+            </div>
+        </div>
+    </div>
+@endsection
