5e397e551ea000b69ec5cf64557832a26d7dabf1
Muhammad Rifki Aufa <rifki_aufa@mhs.unsyiah.ac.id>
2021-05-12 10:39:24 +0700

    Menampilkan data profil

--- ezakat/resources/views/profil/profil.blade.php
+++ ezakat/resources/views/profil/profil.blade.php
@@ -3,23 +3,29 @@
 
 @section('isi')
 
-     <div id="card">
-        <div id="card-content">
-            <div id="card-title">
-            <h1 class="formulir mb-3">Isi Profil</h1>
-            </div>
-            <form method="post" class="form"> 
-              <label>Nama Lengkap</label><br>
-              <input type="text"><br>
-              <label>Nomor Kartu Keluarga</label><br>
-              <input type="text"><br>
-              <label>Jumlah Anggota Keluarga</label><br>
-              <input type="number"><br>
-              <label>Alamat</label><br>
-              <input type="text"><br>
-              <input id="submit-btn" type="submit" name="submit" value="Simpan" />
-            </form>
-          </div>
-     </div>
- 
+    <div id="card">
+    <div id="card-content">
+        <div id="card-title">
+        <h1 class="formulir mb-3">Isi Profil</h1>
+        </div>
+        <form method="post" action="/profil" class="form">
+            @csrf
+
+            <label for='nama'>Nama Lengkap</label><br>
+            <input id='nama' name='nama' type="text" value="{{ old('nama') }}" required autofocus><br>
+
+            <label for='nomorkk'>Nomor Kartu Keluarga</label><br>
+            <input id='nomorkk' name='nomorkk' type="text" value="{{ old('nomorkk') }}" required autofocus><br>
+
+            <label for='jumlah_keluarga'>Jumlah Anggota Keluarga</label><br>
+            <input id='jumlah_keluarga' name='jumlah_keluarga' type="number" value="{{ old('jumlah_keluarga') }}" required autofocus><br>
+
+            <label for='alamat'>Alamat</label><br>
+            <input id='alamat' name='alamat' type="text" value="{{ old('alamat') }}" required autofocus><br>
+
+            <button id="submit-btn" type="submit" name="submit" value="Simpan" />Simpan</button>
+        </form>
+        </div>
+    </div>
+
 @endsection
