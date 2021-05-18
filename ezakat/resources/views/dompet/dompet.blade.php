152e26e80100b4fed74c4683496ed8a5c62b933f
Choirunisa Romadoni <72638934+choirunisa01@users.noreply.github.com>
2021-05-18 22:09:59 +0700

    Mengedit dompet.blade.php

--- ezakat/resources/views/dompet/dompet.blade.php
+++ ezakat/resources/views/dompet/dompet.blade.php
@@ -2,17 +2,35 @@
 @section('title','Dompet')
 
 @section('isi')
-    <div class="container">
-        <h1 class="pt-5" style="color: dodgerblue; font-weight: bold;">Dompet Penerima Zakat</h1>
-        <div class="card mt-5" style="width: 35rem; border-color: dodgerblue; border-radius: 5%; background-color: rgba(14, 159, 221, 0.35);">
+    <div class="container" style="margin-left: 150px;">
+        <h1 class="pt-4" style="color: dodgerblue; font-weight: bold;">Dompet Penerima Zakat</h1>
+        <div class="card mt-4" style="width: 35rem; border-color: dodgerblue; border-radius: 5%; background-color: rgba(14, 159, 221, 0.35); box-shadow: 1px 2px 8px rgba(0, 0, 0, 0.65);">
             <div class="card-body">
-                <h5 class="card-tittle p-3">No KK : 729123498721</h5>
-                <h5 class="card-tittle p-3">Username : Saiful Anwar</h5>
-                <h5 class="card-tittle p-3">Email : saiful@gmail.com</h5>
-                <h5 class="card-tittle p-3">Jumlah Anggota Keluarga : 5</h5>
-                <h5 class="card-tittle p-3">Alamat : Ie Masen</h5>
-                <h5 class="card-tittle p-3">Saldo : Rp 1.500.000</h5>
+                <table>
+                    <tbody>
+                        <tr >
+                            <td> <h5 class="card-tittle p-3">No KK </h5> </td>
+                            <td> <h5 class="card-tittle p-3">:</h5> </td>
+                            <td><h5 class="card-tittle p-3"> 729123498721 </h5></td>
+                        </tr>
+
+                        <tr>
+                            <td> <h5 class="card-tittle p-3">Nama </h5> </td>
+                            <td> <h5 class="card-tittle p-3">:</h5> </td>
+                            <td><h5 class="card-tittle p-3"> Saiful Anwar </h5></td>
+                        </tr>
+
+                        <tr>
+                            <td> <h5 class="card-tittle p-3">Saldo </h5> </td>
+                            <td> <h5 class="card-tittle p-3">:</h5> </td>
+                            <td><h5 class="card-tittle p-3"> Rp 1.500.000 </h5></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div id="box-dompet" >Status Pengiriman : Sudah Terkirim </div>
+                <p class="pt-4" style="color: red; font-weight: bold;">*Zakat akan kami transfer sesuai dengan akun ovo/gopay yang sudah kamu isi sebelumnya.</p>
             </div>
-        </div>
+        </div> 
     </div>
 @endsection
