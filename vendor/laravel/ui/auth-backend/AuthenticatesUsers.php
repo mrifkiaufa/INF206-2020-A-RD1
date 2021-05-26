f99c23d1f90630dd2d7888596eef3f43eeff527f
Muhammad Rifki Aufa <rifki_aufa@mhs.unsyiah.ac.id>
2021-05-26 21:25:09 +0700

    Menambah fitur login untuk admin

--- vendor/laravel/ui/auth-backend/AuthenticatesUsers.php
+++ vendor/laravel/ui/auth-backend/AuthenticatesUsers.php
@@ -31,28 +31,21 @@ trait AuthenticatesUsers
      */
     public function login(Request $request)
     {
-        $this->validateLogin($request);
+        $credentials = $request->only('email', 'password');
 
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if (method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
 
-            return $this->sendLockoutResponse($request);
+            if($request->user()->jenis == 'admin'){
+                return view('admin.home');
+            }
+
+            return redirect('home');
         }
 
-        if ($this->attemptLogin($request)) {
-            return $this->sendLoginResponse($request);
-        }
-
-        // If the login attempt was unsuccessful we will increment the number of attempts
-        // to login and redirect the user back to the login form. Of course, when this
-        // user surpasses their maximum number of attempts they will get locked out.
-        $this->incrementLoginAttempts($request);
-
-        return $this->sendFailedLoginResponse($request);
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
     }
 
     /**
