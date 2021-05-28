663c31270d0f18d48d25de6b5894874e6669dc49
Muhammad-Saifullah-Sani <81971527+Muhammad-Saifullah-Sani@users.noreply.github.com>
2021-05-28 23:32:16 +0700

    Mulai memodifikasi back-end penerima sebagai admin

--- app/Http/Controllers/penerimaController.php
+++ app/Http/Controllers/penerimaController.php
@@ -2,25 +2,133 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-// Buat tabel dlm databse dlu gan baru pkk Models
+use App\Models\Profile;
+use App\Models\User;
+use App\Models\Wallet;
 
-// use App\Models\Profile;
-// use App\Models\User;
-// use App\Models\Wallet;
-// use Illuminate\Support\Facades\Auth;
-// use RealRashid\SweetAlert\Facades\Alert;
-
-class penerimaController extends Controller
+class PenerimaController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(Request $request)
     {
-        if($request->has('cari')) {
-            $data_siswa = \App\Siswa::where('nama', 'LIKE', '%' .$request->cari. '%')->get();
+        if($request->has('search')){
+            $dataProfil = Profile::where('nama','LIKE','%'.$request->search.'%')->get();
         }
-        else {
-            $data_siswa = \App\Siswa::all();
+        else{
+            $dataProfil = Profile::all();
         }
-        return view('siswa.index', ['data_siswa' => $data_siswa]);
+
+        $dataUser =  User::all();
+        return view('admin.penerima.index', compact('dataProfil', 'dataUser'));
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $dataProfil = Profile::where('id_users', $id)->first();
+        $dataUser = User::where('id', $id)->first();
+        return view('admin.penerima.detail', compact('dataProfil', 'dataUser'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Profile::where('id_users', $id)->update([
+            'nama'=> $request->nama,
+            'nomorkk' => $request->nomorkk,
+            'jumlah_keluarga' => $request->jumlah_keluarga,
+            'alamat' => $request->alamat,
+        ]);
+
+        User::where('id', $id)->update([
+            'username'=> $request->username,
+            'email' => $request->email,
+        ]);
+
+        return redirect('/penerima');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::destroy($id);
+        Profile::where('id_users',$id)->delete();
+        $idProfil = Profile::where('id_users',$id)->first()->id;
+        Wallet::where('id_profiles',$idProfil)->delete();
+
+        return redirect('/penerima');
+    }
+
+    public function verif($id)
+    {
+        Profile::where('id_users', $id)->update([
+            'status_bayar'=> 1
+        ]);
+
+        return redirect('/penerima');
+    }
+
+    public function tolak($id)
+    {
+        User::where('id', $id)->update([
+            'tombol_bayar'=> 0
+        ]);
+
+        return redirect('/penerima');
+    }
+
 }
