98da97b4735d48b9d03ef3a660ab66ce7a350050
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2021-05-31 22:19:30 +0700

    Menambah gambar amil pada halaman about dan merapikan kalimat

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -4,12 +4,12 @@
 @section('isi')
     <div class="container ms-3 mt-5">
         <h1 class="artikel">Tentang Kami </h1>
-        <p class="artikel">Website ini dibuat untuk memudahkan <br>
-        proses pembayaran dan penerimaan zakat <br>
-        bagi warga desa Ie Masen</p>
+        <p class="artikel"> Website ini dibuat untuk memudahkan proses pembayaran dan penerimaan zakat <br>
+        bagi warga desa Ie Masen.</p>
 
-        <p class="artikel">Website ini dikelola oleh amil <br>
-        zakat desa Ie Masen<br>
-        </p>
+        <p class="artikel">Website ini dikelola oleh amil zakat desa Ie Masen yaitu arie.<br></p>
+        
+        <img src=" /img/arie.jpeg" style="float:left; margin-left: 10px; border-radius: 20px;" width="240px" height="240px">
+        
     </div>
 @endsection
