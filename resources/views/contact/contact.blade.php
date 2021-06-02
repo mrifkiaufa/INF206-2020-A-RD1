da7f46db46ccac62e9c060653a84f6cd399633c3
ulfiaakhairani <jessika@mhs.unsyiah.ac.id>
2021-06-02 09:34:20 +0700

    Mengubah Tampilan Halaman Contact

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -4,15 +4,15 @@
 @section('isi')
 
 <div class="container ms-3 mt-5">
-    <h1 class="pertama">Hubungi Kami</h1>
+    <h1 class="artikel">Hubungi Kami</h1>
     <br><br>
 
-    <p class="pertama"><img src=" /img/home.png" style="float:left; margin:0 8px 4px 0;" width="35px" height="40px"><b>Jl.Ikhsan, Ie Masen Kayee Adang<br>
+    <p class="artikel"><img src=" /img/home.png" style="float:left; margin:0 8px 4px 0;" width="35px" height="40px"><b>Jl.Ikhsan, Ie Masen Kayee Adang<br>
             Syiah Kuala, Banda Aceh, 24415</b></p>
 
-    <p class="pertama"><img src="/img/email.png" style="float:left; margin:0 8px 4px 0;" width="30px"><b>Email: IeMasen@gmail.com</b></p>
+    <p class="artikel"><img src="/img/email.png" style="float:left; margin:0 8px 4px 0;" width="30px"><b>Email: IeMasen@gmail.com</b></p>
 
-    <p class="pertama"><img src="/img/call.png" style="float:left; margin:0 8px 4px 0;" width="30px" height="30px"><b>Phone: +6282123456789</b></p>
+    <p class="artikel"><img src="/img/call.png" style="float:left; margin:0 8px 4px 0;" width="30px" height="30px"><b>Phone: +6282123456789</b></p>
 </div>
 
 
